155415a1143fd600c3e509609b8cbbe3f8e6d78f
Adam Powers <apowers@ato.ms>
2016-05-12 04:42:21 +0200

    initial express-based server connections

--- comm.js
+++ comm.js
@@ -1,30 +1,63 @@
- module.exports = ServerComm;
+var _ = require("lodash");
+
+module.exports = ServerComm;
 
 /**
  * Implements the ServerComm class
  * Restify-based REST endpoints for communicating with the FIDO Server
- */ 
-function ServerComm(opts) {
+ */
+function ServerComm(server, opt) {
+	var defaults = {
+		loginRoute: "/login",
+		registerRoute: "/register",
+		port: 8088,
+	};
+	opt = _.defaultsDeep(opt, defaults);
+	// TODO: check options
+	this.loginRoute = opt.loginRoute;
+	this.registerRoute = opt.registerRoute;
+	this.port = opt.port;
+	this.server = server;
 }
 
 /**
  * Sets up the communications for the server
  */
 ServerComm.prototype.init = function() {
-	return Promise.resolve (null);
+	var app = express();
+
+	app.use(bodyParser.json());
+
+	app.post(this.registerRoute, function(req, res) {
+		console.log("Register");
+		console.log(req.body);
+		res.redirect("/redirect.html");
+	});
+
+	app.post(this.loginRoute, function(req, res) {
+		console.log("Login");
+		console.log(req.body);
+		res.send(req.body);
+	});
+
+	this.express = app.listen(this.port);
+
+	this.app = app;
+
+	return Promise.resolve(this);
 };
 
 /**
  * Tear down all server communcations
  */
 ServerComm.prototype.shutdown = function() {
-	return Promise.resolve (null);
+	return Promise.resolve(null);
 };
 
-ServerComm.prototype.receive = function (cb) {
-	cb (null, null);
+ServerComm.prototype.receive = function(cb) {
+	cb(null, null);
 };
 
-ServerComm.prototype.send = function (cb) {
-	cb (null, null);
+ServerComm.prototype.send = function(cb) {
+	cb(null, null);
 };
--- example/test-server.js
+++ example/test-server.js
@@ -1,36 +1,35 @@
-var FIDOServer = require ("../server.js");
-var bodyParser = require ("body-parser");
+var FIDOServer = require("../server.js");
+// var bodyParser = require("body-parser");
 var express = require("express");
-var app = express();
 
 var server = new FIDOServer();
-app.use(bodyParser.json ());
+server
+	.init()
+	.then(function() {
+		var app = server.comm.app;
 
-app.get ("/", function (req, res) {
-	console.log ("serving register.html");
-	res.sendFile ("./register.html", {root: "."});
-});
+		app.get("/", function(req, res) {
+			console.log("serving register.html");
+			res.sendFile("./register.html", {
+				root: "."
+			});
+		});
 
-app.get ("/login.html", function (req, res) {
-	console.log ("serving login.html");
-	res.sendFile ("./login.html", {root: "."});
-});
+		app.get("/login.html", function(req, res) {
+			console.log("serving login.html");
+			res.sendFile("./login.html", {
+				root: "."
+			});
+		});
 
-app.get ("/ms-webauth-polyfill.js", function (req, res) {
-	console.log ("serving polyfill");
-	res.sendFile ("./ms-webauth-polyfill.js", {root: "."});
-});
-
-app.post ("/register", function (req, res) {
-	console.log ("Register");
-	console.log(req.body);
-	res.redirect ("/redirect.html");
-});
-
-app.post ("/login", function (req, res) {
-	console.log ("Login");
-	console.log(req.body);
-	res.send (req.body);
-});
-
-app.listen (8088);
+		app.get("/ms-webauth-polyfill.js", function(req, res) {
+			console.log("serving polyfill");
+			res.sendFile("./ms-webauth-polyfill.js", {
+				root: "."
+			});
+		});
+	})
+	.catch(function(err) {
+		console.log ("Couldn't start FIDO server");
+		console.log (err);
+	});
